2931578e021db13e54dc3bb44d0d35ad0105d7e2
Your Name <kangyuchen725@gmail.com>
2025-09-12 08:09:14 +0200

    auth fix

--- backend/middleware/__init__.py
+++ backend/middleware/__init__.py
@@ -1,13 +1,11 @@
 from .auth_middleware import (
     get_current_user,
     get_current_active_user,
-    get_current_superuser,
-    security
+    get_current_superuser
 )
 
 __all__ = [
     "get_current_user",
     "get_current_active_user", 
-    "get_current_superuser",
-    "security"
+    "get_current_superuser"
 ]
